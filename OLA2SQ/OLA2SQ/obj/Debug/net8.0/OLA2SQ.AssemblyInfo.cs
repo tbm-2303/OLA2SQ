6af677041891b93e366d3bacc5500c244316f791
Timothy <timmy.mortensen@hotmail.com>
2024-09-23 16:38:59 +0200

    .

--- OLA2SQ/OLA2SQ/obj/Debug/net8.0/OLA2SQ.AssemblyInfo.cs
+++ OLA2SQ/OLA2SQ/obj/Debug/net8.0/OLA2SQ.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OLA2SQ")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65be00d03e099a499a442d0da3a9f303b220c022")]
 [assembly: System.Reflection.AssemblyProductAttribute("OLA2SQ")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OLA2SQ")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
